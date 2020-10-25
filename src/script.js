085206a711b90394c38c63e0c35680ca2e0857de
Baxoner <baxoner5050@gmail.com>
2020-10-25 09:15:49 +0100

    a

--- src/script.js
+++ src/script.js
@@ -1,15 +1,8 @@
-function sprawdzEmail()
-{
+function sprawdzEmail() {
     let email = document.forms["formularz"]["email"].value;
-    let tresc = document.forms["formularz"]["tresc"].value;
-    if(email.indexOf("zs6sobieski.edu.pl") >=1)
+    if (email.indexOf("@zs6sobieski.edu.pl") >= 1)
     {
-        alert("Ten e-mail jest w w domenie Sobieskiego!");
-        return false;
-    }
-    else
-    {
-        alert("Ten e-mail nie jest w domenie Sobieskiego :(");
+        alert("Ten e-mail jest w domenie Sobieskiego");
         return false;
     }
 }
