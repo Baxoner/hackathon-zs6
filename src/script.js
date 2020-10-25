1aae7549c7793d26e6c2e748aa8fc9fad1d5d601
SzerokiGracjan <gracjan.karpinski@zs6sobieski.edu.pl>
2020-10-25 09:11:12 +0100

    poprawianie dzialania rejestracji

--- src/script.js
+++ src/script.js
@@ -1,9 +1,6 @@
 function sprawdzEmail() {
     let email = document.forms["formularz"]["email"].value;
-    if (email.indexOf("zs6sobieski.edu.pl") >= 1) {
-
-    }
-    else {
+    if (email.indexOf("@zs6sobieski.edu.pl") <= 1) {
         alert("Ten e-mail nie jest w domenie Sobieskiego :(");
         return false;
     }
